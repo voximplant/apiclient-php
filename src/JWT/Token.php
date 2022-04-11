1474efb2683d398d2cd8fd55d562a639afa341d4
Kirill Naumenko <redev1l@pm.me>
2022-04-11 12:35:22 +0300

    Added VOXIMPLANT_CREDENTIALS env var support Added private key caching to reduce disk io

--- src/JWT/Token.php
+++ src/JWT/Token.php
@@ -4,11 +4,22 @@ namespace Voximplant\JWT;
 
 use Exception;
 use Firebase\JWT\JWT;
+use stdClass;
 
 class Token
 {
     /** @var int Min 30 sec, max 3600 sec. */
     public $expTime = 60;
+
+    /** @var stdClass */
+    private $privateKey;
+
+    public function __construct()
+    {
+        if(!empty($_ENV['VOXIMPLANT_CREDENTIALS'])) {
+            $this->privateKey = json_decode($_ENV['VOXIMPLANT_CREDENTIALS']);
+        }
+    }
 
     /**
      * @param string $tokenPath
@@ -19,15 +30,7 @@ class Token
      */
     public function generateAuthorizationHeader($tokenPath): string
     {
-        if (!file_exists($tokenPath)) {
-            throw new Exception('Token file not found!');
-        }
-
-        $content = file_get_contents($tokenPath);
-
-        if (!($privateKey = json_decode($content))) {
-            throw new Exception('JWT authorization error: Token not found!');
-        }
+        $privateKey = $this->getPrivateKey($tokenPath);
 
         $payload = [
             'iat' => time(),
@@ -36,5 +39,27 @@ class Token
         ];
 
         return 'Bearer '.JWT::encode($payload, $privateKey->private_key, 'RS256', $privateKey->key_id);
+    }
+
+    /**
+     * @param $tokenPath
+     * @return stdClass
+     * @throws Exception
+     */
+    private function getPrivateKey($tokenPath)
+    {
+        if (empty($this->privateKey)) {
+            if (!file_exists($tokenPath)) {
+                throw new Exception('Token file not found!');
+            }
+
+            $content = file_get_contents($tokenPath);
+
+            if (!($this->privateKey = json_decode($content))) {
+                throw new Exception('JWT authorization error: Token not found!');
+            }
+        }
+
+        return $this->privateKey;
     }
 }
