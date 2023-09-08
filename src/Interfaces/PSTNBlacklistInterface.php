d7b87e16689611e03f88bacfea316bf284fea1f0
Magomedali <magomedaliev@voximplant.com>
2023-09-08 09:56:05 +0000

    Added method CreateCallList

--- src/Interfaces/PSTNBlacklistInterface.php
+++ src/Interfaces/PSTNBlacklistInterface.php
@@ -5,7 +5,7 @@ namespace Voximplant\Interfaces;
 interface PSTNBlacklistInterface
 {
     /**
-     * @method Add a new phone number to the PSTN blacklist. Blacklist is used to block inbound calls from specified phone numbers to numbers purchased from Voximplant. Since we have no control over exact phone number format for calls from SIP integrations, blacklisting such numbers should be done via JavaScript scenarios.
+     * @method Add a new phone number to the PSTN blacklist. BlackList works for numbers that are purchased from Voximplant only. Since we have no control over exact phone number format for calls from SIP integrations, blacklisting such numbers should be done via JavaScript scenarios.
      */
     public function AddPstnBlackListItem(\Voximplant\Resources\Params\AddPstnBlackListItemParams $params);
 
